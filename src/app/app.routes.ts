e39df74c819eeb2aca1337b53b5d3527b6471f37
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-15 19:19:45 -0300

    Complet
    Troca de tema e rotas funcionais!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/home/home.component';
+import { SobreComponent } from './core/pages/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+];
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ETheme } from '../../../enums/etheme';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  // Variáveis do componente
+  public icon!: ETheme;
 
+  constructor() {
+    this.icon = ETheme.ICON_MOON;
+  }
+
+  public toggle() {
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme) {
+      this.icon = ETheme.ICON_SUN;
+    } else {
+      this.icon = ETheme.ICON_MOON;
+    }
+  }
 }
